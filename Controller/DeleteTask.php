43a091832dd927a3662c71ca41bd83051dbdeed3
dev-mehedi-github <aiubmehedi20@gmail.com>
2026-09-14 00:03:13 +0600

    feat: add DeleteTask controller for task deletion functionality

---
+++ Controller/DeleteTask.php
@@ -0,0 +1,21 @@
+<?php
+include "../Model/db.php";
+session_start();
+
+if(!isset($_SESSION["logged_in"])) {
+    header("Location: ../View/Login.php");
+    exit();
+}
+
+$id = intval($_GET["id"] ?? 0);
+$user_id = $_SESSION["user_id"];
+
+if($id > 0) {
+    $database = new db();
+    $connection = $database->connection();
+    $database->deleteTask($connection, "tasks", $id, $user_id);
+}
+
+header("Location: ../View/Dashboard.php");
+exit();
+?>
