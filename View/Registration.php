4213e0a3196adee15f718818c3a958fa3af71434
dev-mehedi-github <aiubmehedi20@gmail.com>
2026-09-13 16:09:30 +0600

    feat: add html for registration page

--- View/Registration.php
+++ View/Registration.php
@@ -1,66 +1,49 @@
 <?php
-include "../Controller/RegistrationValidation.php";
+require'../Controller/RegistrationValidation.php';
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Registration Page</title>
-    <script>
-        function collect_data()
-        {
-            let name = document.getElementById("name").value.trim();
-            let password = document.getElementById("password").value.trim();
-            let valid = true;
-            let message = "";
-            if (name.length < 5) {
-                message += "User Name Should be 5 Char\n";
-                valid = false;
-            }
-            if (password.length < 5) {
-                message += "Password Must be 5 Char";
-                valid = false;
-            }
-            if (!valid) {
-                alert(message);
-            }
-            return valid;
-        }
-    </script>
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
+    <head>
+        <title>To-Do | Registration</title>
 
-        
-    </style>
-</head>
-<body>
-    <h2>Registration</h2>
-    <form method="post" action="" onsubmit="return collect_data()">
-        <table>
-            <tr>
-                <td><label for="username"> User Name: </label></td>
-                <td><input type="text" id="name" name="name">
-                    <?php echo $name ?>
-                </td>
-            </tr>
-            <tr>
-                <td><label for="pass"> Password: </label></td>
-                <td><input type="password" id="password" name="password">
-                    <?php echo $password ?>
-                </td>
-            </tr>
-            <tr>
+        <script>
+
+        </script>
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <h1>Complete your registration</h1>
+        <form method="post" action="" onsubmit="return collect_data()">
+        <fieldset>
+            <legend>Login Information</legend>
+            <table>
+                <tr>
+                    <td><label for="username"> User Name: </label></td>
+                    <td>
+                        <input type="text" id="name" name="name">
+                        <?php echo htmlspecialchars($name ?? ''); ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="pass"> Password: </label></td>
+                    <td>
+                        <input type="password" id="password" name="password">
+                        <?php echo htmlspecialchars($password ?? ''); ?>
+                    </td>
+                </tr>
+
+                <tr>
                 <td colspan="2">
                     <input type="submit" id="submit" value="Register">
                     <input type="reset" id="reset">
                 </td>
             </tr>
-        </table>
-        <p><?php echo $message; ?></p>
-        <p>Already have an account? <a href="login.php">Login here</a></p>
-    </form>
-</body>
+            </table>
+            <p><?php echo htmlspecialchars($message ?? ''); ?></p>
+            <p>Already have an account? <a href="login.php">Login here</a></p>
+        </fieldset>
+        </form>
+    </body>
 </html>
